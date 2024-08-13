f30cc7d106734ceeb0ea33d26532e7c7a81ad5f1
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-08-13 11:44:45 +0200

    feat: add SectionOne fn

--- src/components/sections/SectionOne.jsx
+++ src/components/sections/SectionOne.jsx
@@ -1,3 +1,32 @@
-export default function SectionOne() {
-  return <h1>Section One</h1>;
+import { useState } from "react";
+import ladyPhoto from "../../assets/images/section-1/lady.jpg";
+
+export default function SectionOne({ baseURL, setSuccessfulMessage }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <section className="section-one">
+        <div className="welcome">
+          <h1>Food Your Way</h1>
+          <p className="welcome-message">
+            Personalize your culinary experience with a versatile app that lets
+            you explore, customize, and save recipes tailored to your taste.
+          </p>
+          <button className="signup-btn" onClick={() => setIsOpen(true)}>
+            Sign Up
+          </button>
+          <div className="rating">
+            <div className="stars">⭐⭐⭐⭐⭐</div>
+            <p>4.8 rating in the AppStore and Google Play</p>
+          </div>
+        </div>
+        <div className="lady-photo">
+          <img src={ladyPhoto} alt="korean lady cooks" />
+        </div>
+      </section>
+
+      {isOpen && <div>SignUpModal</div>}
+    </>
+  );
 }
