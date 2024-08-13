c0c7b58d779c2a07fde87a419d32d4bf088ecfb8
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-08-13 10:53:01 +0200

    feat(auth): implement App and its states

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import "./App.css";
+import { useState } from "react";
 
-function App() {
+// COMPONENTS
+import HomePage from "./components/HomePage";
+import MainPage from "./components/MainPage";
+
+const baseURL = "http://localhost:5000";
+
+export default function App() {
+  const [token, setToken] = useState(localStorage.getItem("jwt"));
+  const [userInfo, setUserInfo] = useState(getUserInfo);
+  const [successfulMessage, setSuccessfulMessage] = useState("");
+
+  function getUserInfo() {
+    return JSON.parse(localStorage.getItem("userInfo"));
+  }
+
   return (
     <>
-      <h1>Samsung Food</h1>
+      {!token ? (
+        <HomePage
+          successfulMessage={successfulMessage}
+          setSuccessfulMessage={setSuccessfulMessage}
+          setToken={setToken}
+          setUserInfo={setUserInfo}
+          baseURL={baseURL}
+        />
+      ) : (
+        <MainPage
+          token={token}
+          setToken={setToken}
+          setSuccessfulMessage={setSuccessfulMessage}
+          userInfo={userInfo}
+          baseURL={baseURL}
+        />
+      )}
     </>
   );
 }
-
-export default App;
