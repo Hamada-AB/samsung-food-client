1df171c4500d87495aa176a98cb2abeb2a7b9418
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-08-14 17:36:03 +0200

    feat(DescriptionBox): add arrow icon

--- src/components/sections/DescriptionBox.jsx
+++ src/components/sections/DescriptionBox.jsx
@@ -1,11 +1,18 @@
+import arrowLink from "../../assets/icons/arrowLink.svg";
+
 export default function DescriptionBox({ h2, p, href, aText, className }) {
   return (
     <div className={className}>
       <h2 className="descBox-h2">{h2}</h2>
       <p>{p}</p>
-      <a href={href} className="section-link">
-        {aText}
-      </a>
+      <div>
+        <a href={href} className="section-link">
+          {aText}
+        </a>
+        <span className="arrow-icon">
+          <img src={arrowLink} alt="arrow icon." />
+        </span>
+      </div>
     </div>
   );
 }
