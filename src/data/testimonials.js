f3dc457eaf2977ec0dda71ae1a5a5056b6054ba4
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-08-15 11:13:01 +0200

    feat(data): declare testimonials obj

---
+++ src/data/testimonials.js
@@ -0,0 +1,131 @@
+export const testimonials = [
+  {
+    title: "on achieving health goals",
+    content:
+      "[Samsung Food] adds the health score, which I find very helpful, and seems to accommodate a wide variety of the latest scientific knowledge on nutrition.",
+    author: "MrWufz",
+  },
+  {
+    title: "on organizing recipes",
+    content:
+      "Answered all of my cooking prayers… no other app could even come close.",
+    author: "Luna",
+  },
+  {
+    title: "on organizing recipes",
+    content: "An incredibly intuitive app that does all the work",
+    author: "Kindness Reviewer",
+  },
+  {
+    title: "on organizing recipes",
+    content:
+      "If you want to build on your own recipe collection and access all your recipes from ONE place… look no further.",
+    author: "Cindix",
+  },
+  {
+    title: "on organizing recipes",
+    content:
+      "Love how easy it is to save recipes from any site, organize and customize.",
+    author: "Jodie",
+  },
+  {
+    title: "on organizing recipes",
+    content:
+      "My recipes were immediately more organized and easily accessible.",
+    author: "Liz",
+  },
+  {
+    title: "on organizing recipes",
+    content:
+      "I really love the ability to click “share to samsung food” instead of printing recipes to pdfs like I had in the past… Honestly, you have no idea how good this app is until you try it.",
+    author: "Julianne",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "I’ve been trying to automate my weekly meal planning… I went as far as trying to build my own app. Then I stumbled upon Samsung Food which has every feature I needed and then some!",
+    author: "Kcboone",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "This is the best recipe and meal planning app–period. I can say this from experience, as I recently tried 8 of the top meal planning apps recommended online, in r/loseit, and other subreddits.",
+    author: "MrWufz",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "Finally, allergy-free meal planning. Being able to create my own recipes and see my week planned out with photos of foods I can actually eat is incredibly useful.",
+    author: "Ruth",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "Everything i was looking for to ease up the frustration of meal planning. It literally cut the work in half for me.",
+    author: "The Mad Gay",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "I love how easy it is to use… this is the most consistent I’ve been with meal planning.",
+    author: "Heather",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "I’ve been looking for an app like this for years! Makes meal planning way easier.",
+    author: "Jillian",
+  },
+  {
+    title: "on better meal planning",
+    content: "This app is the best meal planning app I have ever used.",
+    author: "Gretchen",
+  },
+  {
+    title: "on better meal planning",
+    content:
+      "This app is making meal planning, grocery shopping, and cooking experience so much better. It is saving me hours and serious.",
+    author: "Kelkel1101",
+  },
+  {
+    title: "on saving time shopping",
+    content:
+      "Actually changed my life. Never has it been so easy to make a shopping list.",
+    author: "Jansbebens",
+  },
+  {
+    title: "on saving time shopping",
+    content: "Saves 90% of your time getting the shopping list together.",
+    author: "Peter",
+  },
+  {
+    title: "on discovering recipes",
+    content:
+      "I have had many first time achievements like cooking a pie and baking a vanilla sponge cake. Makes cooking so much more exciting.",
+    author: "Clive",
+  },
+  {
+    title: "on discovering recipes",
+    content:
+      "Really a good app… Having new recipes right there to explore is really awesome.",
+    author: "Autumn",
+  },
+  {
+    title: "on discovering recipes",
+    content:
+      "It’s easy to find new recipes to try and has a variety of different options based on your diet restrictions or disliked foods.",
+    author: "Katie",
+  },
+  {
+    title: "on achieving health goals",
+    content:
+      "I can add my own random recipes and get the nutritional calculations and health score.",
+    author: "Madhavi",
+  },
+  {
+    title: "on achieving health goals",
+    content:
+      "I have expanded my food horizons and lost 14lbs at the same time.",
+    author: "Charlotte",
+  },
+];
