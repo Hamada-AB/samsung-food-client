d6cb592afae30867d0ce3c7073a9986a41f6badd
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-09-07 11:24:34 +0200

    feat(App): replace the url local host with render url

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import HomePage from "./components/HomePage";
 import MainPage from "./components/MainPage";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://samsung-food-server.onrender.com";
 
 export default function App() {
   const [token, setToken] = useState(localStorage.getItem("jwt"));
